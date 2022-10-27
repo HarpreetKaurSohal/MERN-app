b0ff7969f014cc6fb0d3269f52c58239cd697c4a
unknown <harpreet.sohal@kanakasoftware.com>
2022-10-27 14:30:53 +0530

    created homepage with navbar, header and footer

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import { Form, Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">BackPack of Notes</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="m-auto">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">My Notes</Nav.Link>
+            <NavDropdown title="Harpreet Kaur" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
